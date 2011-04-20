0480b4d7fa3c9012f77951f10fce1173d013a0c9
daniel-m <danielmejia55@gmail.com>
2011-04-20 14:35:26 -0500

    fourth commit

--- embedding-main.c
+++ embedding-main.c
@@ -1,10 +1,11 @@
 /*
-  DESCRIPTION: This code consist of a simple embedding process by passing
-               the embedding delay as an argument of the program
+  DESCRIPTION: This code consist of a simple embedding process by passing 
+               the data file as argv[1] and the resulting embedding on
+	       argv[2], also the embedding delay as argv[3] of the program
       
   AUTHOR:      Daniel Mejia Raigosa
   DATE:        16, April 2011
-  VER:         0.1
+  VER:         Makin'
 */
 
 #include <stdio.h>
@@ -33,14 +34,27 @@ main( int argc, char *argv[] ) // when passing arguments
 
 	/* Defining Variables */ 
 
-  char destinationf[20], originf[20],m[5];
+  char destinationfn[20], originfn[20],dinarg[5];
   int dim,longitud,i;
+  
+  FILE *origin;         // File handler
+  FILE *destination;
+
+  /* Starting Main Code */
+
   longitud=strlen(argv[1]);
-  strcpy(originf,argv[1]);
-  printf("originf:");
+  strcpy(originfn,argv[1]);
+  
+  origin=fopen(originfn,"r");
+  if (origin==NULL){
+    printf("\n!!! An Error Has Ocurred During File Reading !!!\n");
+  }
+
+
+  printf("origin file name:");
   for(i=0;i<longitud;i++)
     {
-      printf("%c",originf[i]);
+      printf("%c",originfn[i]);
     }
   printf("\n");
 
